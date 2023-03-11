9fe994902bc9bd0801779a5c242c86fb26638be5
Karastan Vladislav <90850632+Kar1stan@users.noreply.github.com>
2023-03-11 19:18:52 +0300

    Delete src/helpers directory

--- src/helpers/getCurrencyApi..js
+++
@@ -1,6 +0,0 @@
-import Axios from 'axios';
-
-export const getApi = (from) =>{
-        return Axios.get(`https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/${from}.json`)
-           .then((res) => res.data[from])
-}
